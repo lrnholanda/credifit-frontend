f5bd71b63f91efb774938e78b740fbc818bf9a5b
lrnholanda <lrnholanda@gmail.com>
2024-05-21 06:24:03 -0300

    styling some div

--- src/components/Emprestimo/SimulacaoEmprestimo.tsx
+++ src/components/Emprestimo/SimulacaoEmprestimo.tsx
@@ -20,10 +20,19 @@ const handleSimularEmprestimo = async () => {
   }
   setNotification({ message: 'Empréstimo simulado com sucesso!', type: 'success' });
 };
-return {
-  <div> 
+return (
+  <div className="max-w-md mx-auto mt-8"> 
+    <div className="bg-white rounded-md shadow-md overflow-hidden  p-4">
+    <h1 className="text-sm font-semibold text-darkgreen text-left">Simular Empréstimo</h1>
+    <div className="bg-lightorange rounded-md  p-4 m-4 flex items-center">
+        <img className="h-30 w-30 mr-3" src="src/assets/betina-sorrindo_2x 1.svg" alt="betina"/>
+          <h2 className="text-sm font-light text-gray-800 text-center">
+            Você possui saldo para Crédito Consignado pela empresa Seguros Seguradora. Faça uma simulação!
+          </h2>
+        </div>
+    </div>
   </div>
-  }
+);
   
 }
 
